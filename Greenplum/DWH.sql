450a090818238e9ee5147836593f04f607f54448
Mooping <adejmironov@gmail.com>
2026-06-26 15:41:45 +0000

    полная валидация

--- Greenplum/DWH.sql
+++ Greenplum/DWH.sql
@@ -69,24 +69,27 @@ CORE
 DM
 
 
--- Валидация и вставка 
+-- Стадия валидации 
 
-WITH validated AS
-(
-SELECT
-
-*,
-
-user_id ~ '^[0-9]+$' AS valid_user,
-
-age ~ '^[0-9]+$' AS valid_age,
-
-registration_date ~ '^\d{4}-\d{2}-\d{2}$' AS valid_date,
-
-email IS NOT NULL AS valid_email
-
+WITH validated  AS (
+SELECT 
+   user_id,
+    name,
+    age,
+    email,
+    registration_date,
+    load_dttm,
+    batch_id,
+    source_system,
+    
+    user_id ~ '^[0-9]+$' AS valid_user,
+    age ~ '^[0-9]+$' AS valid_age,
+    email IS NOT NULL AS valid_email,
+    registration_date ~ '^\d{4}-\d{2}-\d{2}$' AS valid_date
 FROM raw2.users_raw
-)
+) 
+
+-- ветка Clean
 
 INSERT INTO stg2.users_clean
 
@@ -120,23 +123,48 @@ AND valid_date
 
 AND valid_email;
 
--- Ветка карантина
+-- ветка Quarantine
 
+WITH validated AS (
+    SELECT
+        user_id,
+        name,
+        age,
+        email,
+        registration_date,
+        load_dttm,
+        batch_id,
+        source_system,
+
+        user_id ~ '^[0-9]+$' AS valid_user,
+        age ~ '^[0-9]+$' AS valid_age,
+        email IS NOT NULL AS valid_email,
+        registration_date ~ '^\d{4}-\d{2}-\d{2}$' AS valid_date
+    FROM raw2.users_raw
+)
 INSERT INTO stg2.users_quarantine
-
 SELECT
+    user_id AS user_id_raw,
+    name AS name_raw,
+    age AS age_raw,
+    email AS email_raw,
+    registration_date AS registration_date_raw,
 
-...
+    CASE
+        WHEN NOT valid_user THEN 'invalid user_id'
+        WHEN NOT valid_age THEN 'invalid age'
+        WHEN NOT valid_email THEN 'invalid email'
+        WHEN NOT valid_date THEN 'invalid registration_date'
+        ELSE 'unknown error'
+    END AS error_reason,
 
+    load_dttm,
+    batch_id,
+    source_system
 FROM validated
-
-WHERE NOT
-(
-valid_user
-
-AND valid_age
-
-AND valid_date
-
-AND valid_email
+WHERE NOT (
+    valid_user
+    AND valid_age
+    AND valid_date
+    AND valid_email
 );
