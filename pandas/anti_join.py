35aa22deccb756a9b1b40ca1de242e0e713e0975
Mooping <adejmironov@gmail.com>
2026-09-26 12:28:52 +0000

    anti_join2

--- pandas/anti_join.py
+++ pandas/anti_join.py
@@ -1,5 +1,24 @@
 import pandas as pd
+
+
 # Anti join logic  ~ инвертируем условие
 df1[
     ~df1["id"].isin(df2["id"])
 ] 
+
+
+# Второй способ
+
+def users_without_orders(users, orders):
+    merged = users.merge(
+        orders[["user_id"]],
+        on="user_id",
+        how="left",
+        indicator=True
+    )
+
+    result = merged[
+        merged["_merge"] == "left_only"
+    ][["user_id", "name"]]
+
+    return result  
